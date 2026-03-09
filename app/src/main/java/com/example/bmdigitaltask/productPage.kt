9745d8796e7489cabfa206bc31ce83ca254d6b65
Prateek-x101 <pratikprajapatix101@gmail.com>
2026-03-09 14:39:19 +0530

    BM digital Utilization task 3

--- app/src/main/java/com/example/bmdigitaltask/productPage.kt
+++ app/src/main/java/com/example/bmdigitaltask/productPage.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -20,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,8 +37,8 @@ import coil.compose.AsyncImage
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun productPage(viewModel: viewModel) {
-val productsList = viewModel.productsList.observeAsState(initial = emptyList())
-    val loading = viewModel.loading.observeAsState(initial = true)
+val productsList by viewModel.productsList.observeAsState(initial = emptyList())
+    val loading by viewModel.loading.observeAsState(initial = true)
 
 
 
@@ -107,4 +109,3 @@ fun productItem(product: itemProducts) {
     }
 
 }
-
